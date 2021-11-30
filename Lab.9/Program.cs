2f66c1cd905e03ad9e4749c06a571a22234487a8
VICTORIA TRINOSOVA <trinosova.victoria@yandex.ru>
2021-11-30 13:04:59 +0300

    1

---
+++ Lab.9/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Lab._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("ПРИЛОЖЕНИЕ КАЛЬКУЛЯТОР");
+            Console.Write("Введите целое число X=");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите целое число Y=");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите код операции");
+            Console.WriteLine("1 - сложение");
+            Console.WriteLine("2 - вычитание");
+            Console.WriteLine("3 - умножение");
+            Console.WriteLine("4 - деление");
+            Console.Write("Введите выбор:");
+            int z = 0;
+            try
+            {
+                Console.Write("Введите выбор:");
+                z = Convert.ToInt32(Console.ReadLine());
+                if (z > 4 || z<1)
+                {
+                    throw new Exception("Некорректный ввод");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка:"+ e.Message);
+            }
+            if (z == 1)
+            {
+                int s = x + y;
+                Console.WriteLine(s);
+            }
+            if (z == 2)
+            {
+                int s = x - y;
+                Console.WriteLine(s);
+            }
+            if (z == 3)
+            {
+                int s = x * y;
+                Console.WriteLine(s);
+            }
+            if (z == 4)
+
+            {
+                double s = (double)x / y;
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
+        }
+        
+    }
+}
